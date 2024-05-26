3467c9b734f01c2febb7b643b17398e4fa46ea07
Shomy <26857315+ShomyTKD@users.noreply.github.com>
2024-05-26 20:00:08 +0200

    Domaci 4

--- Predavanje 4/index.js
+++ Predavanje 4/index.js
@@ -17,61 +17,55 @@ localhost:3000/users?id=123
 const http = require('http');
 const url = require('url');
 
-
 ///^\/users\/(\d+)$/
 const routes = [
-    {pattern: /^\/users\/(\d+)$/, handler: getUserById},
-    {pattern: /^\/products$/, handler: getProducts}
+    { pattern: /^\/users\/(\d+)$/, handler: getUserById },
+    { pattern: /^\/products$/, handler: getProducts }
 ];
 
 const server = http.createServer((req, res) => {
+    const parsedUrl = url.parse(req.url, true);
+    //Proveravamo rute
+    for (const route of routes) {
+        const match = route.pattern.exec(parsedUrl.pathname);
+        if (match) {
+            return route.handler(req, res, match);
+        }
 
-        const parsedUrl = url.parse(req.url, true);
-        //Proveravamo rute
-        for(const route of routes)
-            {
-                const match = route.pattern.exec(parsedUrl.pathname);
-                if(match)
-                    {
-                        return route.handler(req, res, match);
-                    } 
-
-            }
-            //GetCars(req,res)
-            //Ako ruta nije pronadjena vracamo 404
-            /* 
-            res.statusCode = 200;
-            res.setHeader('Content-Type','text/plain');
-            */
-            res.writeHead(404, {'Content-Type': 'text/plain'});
-            res.end('Not found');
+    }
+    //GetCars(req,res)
+    //Ako ruta nije pronadjena vracamo 404
+    /* 
+    res.statusCode = 200;
+    res.setHeader('Content-Type','text/plain');
+    */
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Not found');
 });
+
 //Definisemo handler-e
-function getUserById(req, res, match)
-{
+function getUserById(req, res, match) {
     const userId = match[1];
-    res.writeHead(200, {'Content-Type': 'application/json'});
-    res.end(JSON.stringify({ IdKorisnika : userId}))
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ IdKorisnika: userId }))
 
 };
 
-function getProducts(req, res)
-{
-    
-    res.writeHead(200, {'Content-Type': 'application/json'});
-    res.end(JSON.stringify({ Proizvodi : []}))
+function getProducts(req, res) {
+
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ Proizvodi: [] }))
 };
-server.listen(3000, ()=>{
+server.listen(3000, () => {
     console.log('Server working on port 3000');
 });
-
 
 //Deljenje na infrastrukturalni i aplikativni deo nasu aplikaciju
 const app = require('./app');
 
 const server2 = http.createServer(app);
 
-server2.listen(3001, ()=>{
+server2.listen(3001, () => {
     console.log('Server working on port 3001');
 });
 
@@ -123,13 +117,51 @@ if (match) {
 }
 */
 
-
-
-
 /* Domaci rad
 - Napisati 10 različitih regularnih izraza - potraziti na web-u stranice za Regex
 - Preneti funkcionalnosti iz prethodnog domaćeg zadatka u novi projekat sa novim ruterom
 */
+
+// Regex izrazi
+// Provera se vrši sa .text ili .match funcijom
+
+/* Provera da li string sadrži samo slova i brojeve 
+/^[a-zA-Z0-9]+$/;
+*/
+
+/* Provera da li string sadrži samo slova
+    /^[a-zA-Z]+$/
+*/
+
+/* Provera da li string sadrži samo mala slova
+/^[a-z]+$/;
+*/
+
+/* Provera da li string sadrži samo velika slova
+/^[A-Z]+$/;
+*/
+
+/* Provera da li je string URL adresa
+/^(https?:\/\/)?((([a-zA-Z\d]([a-zA-Z\d-]*[a-zA-Z\d])*)\.)+[a-zA-Z]{2,}|((\d{1,3}\.){3}\d{1,3}))(:\d+)?(\/[-a-zA-Z\d%_.~+]*)*(\?[;&a-zA-Z\d%_.~+=-]*)?(#[-a-zA-Z\d_]*)?$/;
+*/
+
+/* Provera da li su u stringu samo brojevi
+/^[0-9]+$/
+*/
+
+/* Provera da li je string validan email
+/^[^\s@]+@[^\s@]+\.[^\s@]+$/
+*/
+
+// Usecase scenario
+const hasOnlyLetters = (str) => {
+    const regex = /^[a-zA-Z]+$/;
+    return regex.test(str);
+}
+
+console.log(hasOnlyLetters("abc")); // true
+console.log(hasOnlyLetters("abc123")); // false
+
 
 
 //https://regex101.com/
